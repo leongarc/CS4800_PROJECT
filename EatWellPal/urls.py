8fa430184f4b01088325887d8982c9041aede5c4
Luis Ochoa <77687675+LuisOchoa112@users.noreply.github.com>
2023-11-11 20:10:44 -0800

    Website Prototype
    First look at the website

--- EatWellPal/urls.py
+++ EatWellPal/urls.py
@@ -18,6 +18,6 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
-    path('home/', include("home.urls")),
     path('admin/', admin.site.urls),
+    path('home/', include("home.urls"))
 ]
--- home/views.py
+++ home/views.py
@@ -5,4 +5,4 @@ from django.http import HttpResponse
 
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the home index.")
+    return render(request, "home/index.html")
