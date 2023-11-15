b1f06a3d9222eb77a99a2d93c82b1b1ae5b5514b
Jack Wheeland <109243756+jackwheeland@users.noreply.github.com>
2023-11-15 09:09:40 -0800

    Update progress_page.py

--- protoype_v2/progress_page.py
+++ protoype_v2/progress_page.py
@@ -7,87 +7,58 @@
 
 import sqlite3
 import matplotlib.pyplot as plt
-import numpy as np
 
-# Function to display a text-based progress bar
-def print_progress_bar(current, total, length=50):
-    progress = int(length * current / total)
-    bar = "[" + "=" * progress + " " * (length - progress) + "]"
-    percent = f"{(current / total) * 100:.2f}%"
-    print(f"{bar} {percent} - {current}/{total}")
+class ProgressBar:
+    def __init__(self, current, total, length=50):
+        self.current = current
+        self.total = total
+        self.length = length
 
-# Function to plot weight progress chart
-def plot_weight_progress(user_id):
-    conn = sqlite3.connect("weight_progress.db")
-    cursor = conn.cursor()
+    def display(self):
+        progress = int(self.length * self.current / self.total)
+        bar = "[" + "=" * progress + " " * (self.length - progress) + "]"
+        percent = f"{(self.current / self.total) * 100:.2f}%"
+        print(f"{bar} {percent} - {self.current}/{self.total}")
 
-    cursor.execute("SELECT date, weight FROM weight_data WHERE user_id=?", (user_id,))
-    data = cursor.fetchall()
+class WeightProgressChart:
+    def __init__(self, user_id):
+        self.user_id = user_id
 
-    dates = [entry[0] for entry in data]
-    weights = [entry[1] for entry in data]
+    def plot(self):
+        conn = sqlite3.connect("weight_progress.db")
+        cursor = conn.cursor()
 
-    plt.plot(dates, weights, marker='o', linestyle='-', color='b')
-    plt.xlabel("Date")
-    plt.ylabel("Weight")
-    plt.title("Weight Progress Chart")
-    plt.xticks(rotation=45)
-    plt.grid(True)
-    plt.tight_layout()
-    plt.show()
+        cursor.execute("SELECT date, weight FROM weight_data WHERE user_id=?", (self.user_id,))
+        data = cursor.fetchall()
 
-def main(user_id):
-    # Connect to the database and retrieve calorie consumption data
-    conn = sqlite3.connect("calorie_intake.db")
-    cursor = conn.cursor()
+        dates = [entry[0] for entry in data]
+        weights = [entry[1] for entry in data]
 
-    cursor.execute("SELECT consumed_calories, daily_calorie_goal FROM calorie_data WHERE user_id=?", (user_id,))
-    data = cursor.fetchone()
+        plt.plot(dates, weights, marker='o', linestyle='-', color='b')
+        plt.xlabel("Date")
+        plt.ylabel("Weight")
+        plt.title("Weight Progress Chart")
+        plt.xticks(rotation=45)
+        plt.grid(True)
+        plt.tight_layout()
+        plt.show()
 
-    if data:
-        consumed_calories, daily_calorie_goal = data
-        print(f"Consumed Calories: {consumed_calories} / Daily Goal: {daily_calorie_goal}")
-        print("Calorie Consumption Progress:")
-        print_progress_bar(consumed_calories, daily_calorie_goal)
-    else:
-        print("No calorie consumption data available.")
+class UserInterface:
+    def __init__(self, user_id):
+        self.user_id = user_id
 
-    # Plot weight progress chart
-    plot_weight_progress(user_id)
-    
-    #Displays the different options a user can do like nothing or 
-    #change account info     
-    print("6. Home Page")
-    print("8. Favorites Page")
-    print("9. Account Page")
-    print("0. Quit App")
+    def display_calorie_progress(self):
+        conn = sqlite3.connect("calorie_intake.db")
+        cursor = conn.cursor()
 
-    response = int(input("What would you like to do?\n"))
-    
-    #if the user chooses to change info, then gets new info from user
-    if response == 1:
-        pass
-    elif (response in range(6,10)) or (response == 0):
-        return(response)
-        
-    else:
-        print(response)
-        print("Your response didn't match the desired input. \nPlease choose one of the options above.")
+        cursor.execute("SELECT consumed_calories, daily_calorie_goal FROM calorie_data WHERE user_id=?", (self.user_id,))
+        data = cursor.fetchone()
 
-    
-    
-
-# if __name__ == "__main__":
-#     user_id = input("Enter your user ID: ")
-
-#     try:
-#         user_id = int(user_id)
-#     except ValueError:
-#         print("Invalid user ID. Please enter a valid user ID.")
-#     else:
-#         main(user_id)
-
-
-#A function that ask the user if they want to switch tabs ie Progress, Account, Favorite or sign out/quit application
-#Have their response return  to the main page
-#6. Home, 7. Progress, 8. Favorite, 9. Account, 0. Quit
+        if data:
+            consumed_calories, daily_calorie_goal = data
+            print(f"Consumed Calories: {consumed_calories} / Daily Goal: {daily_calorie_goal}")
+            print("Calorie Consumption Progress:")
+            progress_bar = ProgressBar(consumed_calories, daily_calorie_goal)
+            progress_bar.display()
+        else:
+            print("No calorie consumption data available")
