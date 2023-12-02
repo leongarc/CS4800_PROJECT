324b4610070bb525c018a21944fa5eb4be4773e3
Leo <lgarcia109@csustan.edu>
2023-12-01 19:23:04 -0800

    Added favoritescolumn in users.db

--- EatWellPal FLASK/connectors/favorites_db_connector.py
+++ EatWellPal FLASK/connectors/favorites_db_connector.py
@@ -10,11 +10,18 @@ class FavoritesDBConnector:
 
     def __init__(self, path=db_path):
         self.user_db = path
-        self.conn = sqlite3.connect(self.user_db)
+        self.conn = self.connect_to_db()
         self.cur = self.conn.cursor()
+    
+    def connect_to_db(self):
+        try:
+            conn = sqlite3.connect(self.user_db)
+            return conn
+        except OSError:
+            print("DB does not exist/Error opening")
 
     def get_favorites(self, user_id):
-        # need to create favorites column
+        # need to create favorites column in users.db
         # need to set up favorites data in a JSON format since arrays aren't suppported in SQLite
         self.cur.execute("SELECT favorites FROM userinfo WHERE user_id = ?", (str(user_id)))
         result = self.cur.fetchone()
@@ -23,7 +30,7 @@ class FavoritesDBConnector:
         else:
             result = None
             return result
-        
+    # Still need to work on update_favorites
     def update_favorites():
         pass
 
