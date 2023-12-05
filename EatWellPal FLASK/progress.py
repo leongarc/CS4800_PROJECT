f95e454c36081d929b3a9928d39aef287f3c8300
Jack Wheeland <109243756+jackwheeland@users.noreply.github.com>
2023-12-05 13:50:49 -0800

    Update progress.py

--- EatWellPal FLASK/progress.py
+++ EatWellPal FLASK/progress.py
@@ -5,7 +5,62 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-# Define your ProgressBar, WeightProgressChart, and UserInterface classes here
+# Class ProgressBar, Weight ProgressChart and UserInt
+
+class ProgressBar:
+    def __init__(self, current, total, length=50):
+        self.current = current
+        self.total = total
+        self.length = length
+
+    def display(self):
+        progress = int(self.length * self.current / self.total)
+        bar = "[" + "=" * progress + " " * (self.length - progress) + "]"
+        percent = f"{(self.current / self.total) * 100:.2f}%"
+        print(f"{bar} {percent} - {self.current}/{self.total}")
+
+class WeightProgressChart:
+    def __init__(self, user_id):
+        self.user_id = user_id
+
+    def plot(self):
+        conn = sqlite3.connect("database.db")
+        cursor = conn.cursor()
+
+        cursor.execute("SELECT date, weight FROM weight_data WHERE user_id=?", (self.user_id,))
+        data = cursor.fetchall()
+
+        dates = [entry[0] for entry in data]
+        weights = [entry[1] for entry in data]
+
+        plt.plot(dates, weights, marker='o', linestyle='-', color='b')
+        plt.xlabel("Date")
+        plt.ylabel("Weight")
+        plt.title("Weight Progress Chart")
+        plt.xticks(rotation=45)
+        plt.grid(True)
+        plt.tight_layout()
+        plt.show()
+
+class UserInterface:
+    def __init__(self, user_id):
+        self.user_id = user_id
+
+    def display_calorie_progress(self):
+        conn = sqlite3.connect("database.db")
+        cursor = conn.cursor()
+
+        cursor.execute("SELECT consumed_calories, daily_calorie_goal FROM calorie_data WHERE user_id=?", (self.user_id,))
+        data = cursor.fetchone()
+
+        if data:
+            consumed_calories, daily_calorie_goal = data
+            print(f"Consumed Calories: {consumed_calories} / Daily Goal: {daily_calorie_goal}")
+            print("Calorie Consumption Progress:")
+            progress_bar = ProgressBar(consumed_calories, daily_calorie_goal)
+            progress_bar.display()
+        else:
+            print("No calorie consumption data available")
 
 @app.route('/')
 def index():
